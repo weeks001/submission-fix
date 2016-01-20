aff499daf4ad9d35800978226d9459a986cce9d6
Marie Weeks <lightningzero18@gmail.com>
2016-01-19 22:19:09 -0500

    Fixed unit tests.

--- UnitTests.py
+++ UnitTests.py
@@ -26,7 +26,7 @@ class TestSubfixMethods(unittest.TestCase):
 	def test_readCSVSubset(self):
 		answer = ['SNAKE, SOLID', 'SNAKE, LIQUID', 'BOSS, BIG', 'OCELOT, REVOLVER', 
 				  'SILVERBURGH, MERYL', 'HUNTER, NAOMI', 'CAMPBELL, ROY']
-		self.assertEqual(SubmissionFix.readCSV('testingcsv1.csv'), answer)
+		self.assertEqual(SubmissionFix.TSquare().readCSV('testingcsv1.csv'), answer)
 
 	def test_readCSVAll(self):
 		answer = ['SNAKE, SOLID', 'SNAKE, LIQUID', 'SNAKE, SOLIDUS', 'BOSS, BIG', 
@@ -34,66 +34,28 @@ class TestSubfixMethods(unittest.TestCase):
 				  'SILVERBURGH, MERYL', 'WOLF, SNIPER', 'RAVEN, VULCAN', 'MANTIS, PSYCHO',
 				  'OCTOPUS, DECOY', 'CAMPBELL, ROY', 'ANDERSON, DONALD', 'BAKER, KENNETH',
 				  'MILLER, KAZUHIRA', 'LING, MEI', 'ROMANENKO, NASTASHA', 'SASAKI, JOHNNY']
-		self.assertEqual(SubmissionFix.readCSV('testingcsv2.csv'), answer)
+		self.assertEqual(SubmissionFix.TSquare().readCSV('testingcsv2.csv'), answer)
 
 	def test_readCSVOverlap(self):
 		answer = ['SNAKE, SOLIDUS', 'SNAKE, SOLID']
-		self.assertEqual(SubmissionFix.readCSV('testingcsv3.csv'), answer)
+		self.assertEqual(SubmissionFix.TSquare().readCSV('testingcsv3.csv'), answer)
 
 	def test_readCSVOne(self):
 		answer = ['FOX, GREY']
-		self.assertEqual(SubmissionFix.readCSV('testingcsv4.csv'), answer)
+		self.assertEqual(SubmissionFix.TSquare().readCSV('testingcsv4.csv'), answer)
 
 	#stripTime
 	@unittest.skipIf(findTime is False, "pytz needed for this test")
 	def test_stripTimeNormal(self):
 		eastern = timezone('US/Eastern')
 		answer = datetime.datetime(2005,02,28,17,15)  #should be 5 hours behind result?
-		self.assertEqual(SubmissionFix.stripTime('20050228221512345'), eastern.localize(answer))
+		self.assertEqual(SubmissionFix.TSquare().stripTime('20050228221512345'), eastern.localize(answer))
 
 	@unittest.skipIf(findTime is False, "pytz needed for this test")
 	def test_stripTimeTimeZoneBehindDay(self):
 		eastern = timezone('US/Eastern')
 		answer = datetime.datetime(2005,02,27,22,30) 
-		self.assertEqual(SubmissionFix.stripTime('20050228033012345'), eastern.localize(answer))
-
-	#helper functions
-	@contextmanager
-	def suppressOutput(self):
-		with open(os.devnull, 'w') as devnull:
-			oldstdout = sys.stdout
-			sys.stdout = devnull
-			yield
-			sys.stdout = oldstdout
-
-	@contextmanager
-	def tempTestDir(self, args, test, answer):
-		with self.tempDirectory() as path:
-			with self.integrationContentsTest(args, path, test, answer) as result:
-				yield result
-			
-	@contextmanager
-	def tempDirectory(self):
-		path = os.path.abspath('test_folder')
-		if not os.path.exists(path):
-			os.makedirs(path)
-		yield path
-		shutil.rmtree(path)
-
-	@contextmanager
-	def integrationContentsTest(self, args, path, test, answer):
-		shutil.copy(os.path.abspath('testing_set.zip'), path)
-		base = os.getcwd()
-		os.chdir(path)
-		with self.suppressOutput():
-			SubmissionFix.main(args)
-		yield self.existingPathsTest(base, test, answer)
-		os.chdir(base)
-
-	def existingPathsTest(self, base, test, paths):
-		self.logTest(base, test, ['[{exists}]  {path}\n'.format(exists=str(os.path.exists(p)), path=p) for p in paths])
-		return all([os.path.exists(p) for p in paths])	
-
+		self.assertEqual(SubmissionFix.TSquare().stripTime('20050228033012345'), eastern.localize(answer))
 
 if __name__ == '__main__' :
 	suite = unittest.TestLoader().loadTestsFromTestCase(TestSubfixMethods)
